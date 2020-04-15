50b39b5f9c8e30b3262cab41273e25ff3731a514
Mafuo3HuK <kerjackov.sochi2014@yandex.ru>
2020-04-15 18:53:49 +0300

    Memory game

---
+++ Assets/Memory game assets/Scripts/UIButton.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIButton : MonoBehaviour
+{
+    [SerializeField] private GameObject targetObject;
+    [SerializeField] private string targetMessage;
+    public Color highlightColor = Color.grey;
+
+    public void OnMouseEnter()
+    {
+        SpriteRenderer sprite = GetComponent<SpriteRenderer>();
+        if (sprite != null)
+        {
+            sprite.color = highlightColor;
+        }
+    }
+    public void OnMouseExit()
+    {
+        SpriteRenderer sprite = GetComponent<SpriteRenderer>();
+        if (sprite != null)
+        {
+            sprite.color = Color.white;
+        }
+    }
+    public void OnMouseDown()
+    {
+        transform.localScale = new Vector3(0.09f, 0.09f, 0.09f);
+    }
+    public void OnMouseUp()
+    {
+        if (targetObject != null)
+        {
+            targetObject.SendMessage(targetMessage);
+        }
+    }
+}
